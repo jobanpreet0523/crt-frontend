133128fc3dfc626cd3eacae70d5894b2a510ebee
jobanpreet0523 <jobanpreet0523@gmail.com>
2026-05-30 09:44:55 +0530

    Fix script module extension routing

---
+++ crt-dashboard/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DojiScreener from './DojiScreener';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DojiScreener />
+  </React.StrictMode>
+);
